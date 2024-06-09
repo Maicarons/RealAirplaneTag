ff33113441f27b24af1659ea7787be934c4ccd5f
Maicarons <136282328+Maicarons@users.noreply.github.com>
2024-06-09 11:58:55 +0800

    tag: v0.1.2

---
+++ MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RealAirplaneTag
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "RealAirplaneTag";
+        public const string PLUGIN_NAME = "RealAirplaneTag";
+        public const string PLUGIN_VERSION = "0.1.2";
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -15,6 +15,8 @@ using BepInEx.AssemblyPublicizer;
 namespace RealAirplaneTag
 {
     [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    
+    
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
