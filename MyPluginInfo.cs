f75428dbacf98fc739ddf6b1614c03bfb6d8e696
Maicarons <136282328+Maicarons@users.noreply.github.com>
2024-06-09 13:04:01 +0800

    tag:v0.2.2

--- MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace RealAirplaneTag
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "RealAirplaneTag";
-        public const string PLUGIN_NAME = "RealAirplaneTag";
-        public const string PLUGIN_VERSION = "0.1.2";
-    }
-}
